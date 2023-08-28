e68b6af4eb00c9343621a85fda76e9b641b4dbfa
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-28 11:16:52 +0900

    update

--- src/main/java/com/example/demo/controllers/RestSampleController.java
+++ src/main/java/com/example/demo/controllers/RestSampleController.java
@@ -4,20 +4,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 public class RestSampleController {
-    
-    @GetMapping(value="/hello")
+
+    // http://localhost:8080/hello
+    @GetMapping(value = "/hello")
     public String index() {
         return "Hello, Spring";
     }
 
-    @GetMapping(value="/bye")
-    public String bye(@RequestParam String param) {
+    // http://localhost:8080/bye?name=xxxxx
+    @GetMapping(value = "/bye")
+    // public String bye(@RequestParam String name) {
+    public String bye(@RequestParam(name = "name", required = false) String param) {
         String message = "Bye!" + param;
         return message;
     }
-    
-    
+
 }
