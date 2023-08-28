a56abc8d52a6d3c073f15e4a6f45e66abb9de3dd
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-28 13:20:20 +0900

    add AdminHomeController

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -3,7 +3,7 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller("HomeController")
 public class HomeController {
 
     //Webルートにルーティング
---
+++ src/main/java/com/example/demo/controllers/admin/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller("AdminHomeController")
+// URLパターンで「/admin/」 は共通
+@RequestMapping("/admin/")
+public class HomeController {
+    
+    @GetMapping(value="")
+    public String index() {
+        return "admin/index";
+    }
+    
+}
