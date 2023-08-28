70bcfd204b16155ba0c73e6d2135c8ca87963737
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-28 14:33:49 +0900

    add ArticleController

---
+++ src/main/java/com/example/demo/controllers/ArticleController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller("ArticleController")
+public class ArticleController {
+    
+    @GetMapping(value="/article/{id}")
+    public String detail(@PathVariable("id") Long id) {
+        System.out.println("ID is " + id);
+        return "article/index";
+    }
+    
+}
