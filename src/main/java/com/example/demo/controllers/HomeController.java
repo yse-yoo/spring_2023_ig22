1c87aa28a08481b6e3efb639e42229df8b4da5a2
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-28 11:47:56 +0900

    update

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = "/")
+    @GetMapping("/")
     @ResponseBody
     public String index() {
         return "This is Top page.";
