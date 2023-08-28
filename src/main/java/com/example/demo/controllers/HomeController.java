c1fa181bb8f3522fdb095457c2eb56bd49b70e92
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-28 12:01:49 +0900

    update

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -2,15 +2,14 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    //reosuces/templates/home/sample.html を表示
     @GetMapping("/")
-    @ResponseBody
     public String index() {
-        return "This is Top page.";
+        return "home/sample";
     }
 
 }
