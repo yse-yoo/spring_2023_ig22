c5bd0e69c4302a82ac36d080b705a29799effe54
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-11 14:12:09 +0900

    add article index

--- src/main/java/com/example/demo/controllers/ArticleController.java
+++ src/main/java/com/example/demo/controllers/ArticleController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller("ArticleController")
 public class ArticleController {
     
+    @GetMapping(value="/article/")
+    public String index() {
+        return "article/index";
+    }
+
     @GetMapping(value="/article/{id}")
     public String detail(@PathVariable("id") Long id) {
         System.out.println("ID is " + id);
